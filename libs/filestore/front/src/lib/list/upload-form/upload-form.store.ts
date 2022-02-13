f81eeefee7344be29ce37047b50979475c7728d8
Maksim Gordeev <mxmgrd@gmail.com>
2022-02-13 05:08:44 +0300

    Upload form store

---
+++ libs/filestore/front/src/lib/list/upload-form/upload-form.store.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { SettingsFacade } from '@jellyblog-nest/settings/front';
+import { SettingName } from '@jellyblog-nest/utils/common';
+import { Observable, tap } from 'rxjs';
+import { FilestorelistFacade } from '../store/filestore-list.facade';
+import { UploadEvent } from '@jellyblog-nest/utils/front-file-uploader';
+import { GlobalActions, GlobalToastSeverity } from '@jellyblog-nest/utils/front';
+import { Store } from '@ngrx/store';
+
+export interface UploadFormState {
+  prefix: string;
+  revealOriginalFileName: boolean;
+}
+
+const initialState: UploadFormState = {
+  prefix: '',
+  revealOriginalFileName: false,
+};
+
+@Injectable()
+export class UploadFormStore extends ComponentStore<UploadFormState> {
+  constructor(
+    private readonly settingsFacade: SettingsFacade,
+    private readonly filelistFacade: FilestorelistFacade,
+    private readonly globalStore: Store,
+  ) {
+    super(initialState);
+
+    // Когда в списке меняют "директорию" - установить такую же здесь
+    this.setPrefix(
+      this.filelistFacade.prefix$,
+    );
+  }
+
+  readonly s3Bucket$: Observable<string | null | undefined> = this.settingsFacade.getSetting$(SettingName.S3_BUCKET);
+  readonly s3Config$ = this.settingsFacade.s3ClientConfig$;
+  readonly prefix$ = this.select(state => state.prefix);
+  readonly revealOriginalFileName$ = this.select(state => state.revealOriginalFileName);
+
+  readonly setPrefix = this.updater((state, prefix: string) => {
+    return {
+      ...state,
+      prefix,
+    };
+  });
+
+  readonly setRevealOriginalFileName = this.updater((state, revealOriginalFileName: boolean) => {
+    return {
+      ...state,
+      revealOriginalFileName,
+    };
+  });
+
+  readonly handleUploadEvents = this.effect((event$: Observable<UploadEvent>) => {
+    return event$.pipe(
+      tap((event) => {
+        switch (event.type) {
+          case 'UploadBegin': {
+            this.globalStore.dispatch(GlobalActions.addGlobalToast({
+              text: `${event.fileInfo.name} uploading`,
+              severity: GlobalToastSeverity.INFO,
+            }));
+            break;
+          }
+          case 'UploadError': {
+            this.globalStore.dispatch(GlobalActions.addGlobalToast({
+              text: event.errorInfo.message,
+              severity: GlobalToastSeverity.ERROR,
+            }));
+            break;
+          }
+          case 'UploadSuccess': {
+            this.globalStore.dispatch(GlobalActions.addGlobalToast({
+              text: `${event.fileInfo.name} uploaded`,
+              severity: GlobalToastSeverity.SUCCESS,
+            }));
+            // TODO notify filelist store after upload all pending files
+          }
+        }
+      }),
+    );
+  });
+}
