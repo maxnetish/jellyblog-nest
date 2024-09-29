7fad0dfa7ff621a9544d1ba1bd62f0d02386d95e
Maksim Gordeev <mxmgrd@gmail.com>
2024-09-30 02:18:58 +0300

    Remove last angular module

--- libs/auth/front/src/index.ts
+++ libs/auth/front/src/index.ts
@@ -1,1 +1,8 @@
-export * from './lib/auth-front.module';
+export * from './lib/store/auth.effects';
+export * from './lib/store/auth.facade';
+export * from './lib/login-form/login-form.component';
+export * from './lib/auth.guard';
+export * from './lib/auth.service';
+export * from './lib/login-form/login-form-modal.service';
+export  * as AuthActions from './lib/store/auth.actions';
+export * as AuthReducer from './lib/store/auth.reducer';
