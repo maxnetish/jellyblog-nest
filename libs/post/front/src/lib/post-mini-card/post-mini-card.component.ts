e8225e75e5bc868cf41a61dfa0cef6712d6e5a7e
maxnetish <mxmgrd@gmail.com>
2024-10-05 14:22:49 +0300

    Post mini card p.2

--- libs/post/front/src/lib/post-mini-card/post-mini-card.component.ts
+++ libs/post/front/src/lib/post-mini-card/post-mini-card.component.ts
@@ -2,12 +2,23 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { PostShortDto } from '@jellyblog-nest/post/model';
 import { NativeDatePipe } from '@jellyblog-nest/utils/front';
 import { postStatusMap } from '@jellyblog-nest/utils/common';
+import { postPermissionMap } from '../../../../../utils/common/src/lib/post-permission';
+import { NgIcon, NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroArrowTopRightOnSquare, heroPencil, heroPencilSquare } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-post-mini-card',
   standalone: true,
   imports: [
     NativeDatePipe,
+    NgIcon,
+    NgIconComponent,
+  ],
+  providers: [
+    provideIcons({
+      heroPencil,
+      heroArrowTopRightOnSquare,
+    }),
   ],
   templateUrl: './post-mini-card.component.html',
   styleUrl: './post-mini-card.component.scss',
@@ -16,4 +27,5 @@ import { postStatusMap } from '@jellyblog-nest/utils/common';
 export class PostMiniCardComponent {
   readonly post = input<PostShortDto>();
   protected readonly postStatusMap = postStatusMap;
+  protected readonly postPermissionMap = postPermissionMap;
 }
--- libs/utils/common/src/lib/post-permission.ts
+++ libs/utils/common/src/lib/post-permission.ts
@@ -3,3 +3,18 @@ export enum PostPermission {
   FOR_REGISTERED = 'FOR_REGISTERED',
   FOR_ME = 'FOR_ME',
 }
+
+export const postPermissionMap: Record<PostPermission, { label: string; badgeClass: string; }> = {
+  [PostPermission.FOR_ALL]: {
+    label: 'Для всех',
+    badgeClass: 'text-bg-success',
+  },
+  [PostPermission.FOR_ME]: {
+    label: 'Только мне',
+    badgeClass: 'text-bg-secondary',
+  },
+  [PostPermission.FOR_REGISTERED]: {
+    label: 'Для зарегистрированных',
+    badgeClass: 'text-bg-warning',
+  },
+}
